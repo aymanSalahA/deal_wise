d2c646f3064c43953f032f1631c40f9eed53e328
Ayman Salah <ayman.1551.salah@gmail.com>
2025-11-03 19:15:42 +0200

    Phase 1 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:deal_wise/features/auth/data/api_service/otp_service.dart';
 import 'package:deal_wise/features/auth/presentation/cubit/login_cubit.dart';
 import 'package:deal_wise/features/auth/presentation/cubit/otp_verification_cubit.dart';
 
-// Tag 1
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
